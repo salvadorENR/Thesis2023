091a9792783718cdb689a5d1ca6bfe2be20add8e
Salvador <salvadorestadistic@gmail.com>
2023-02-08 22:35:08 +0900

    This is the second commit

---
+++ MultilevelRegression.R
@@ -0,0 +1,11 @@
+#Loading packages
+library(datos)
+library(ggplot2)
+library(readr)
+library(scales)
+library(stringr)
+library(utf8)
+library(devtools)
+library(ggpubr)
+#Loading data base
+DBT=read_csv2("DB_WS1.csv")
